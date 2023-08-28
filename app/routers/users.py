942a9d6bfc5af3b5f4a7dc1ad5ca5cc22388dd42
Austin O'Neil <austin.oneil@redcanary.com>
2023-08-28 00:11:45 -0400

    boilerplate

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,58 +1,146 @@
-from fastapi import (
-    APIRouter,
-    Depends,
-    status,
-    Request,
-    Form,
-    BackgroundTasks,
-    HTTPException,
-    Security,
-    Header,
-)
-import jwt
-import secrets
-import bcrypt
 from datetime import datetime, timedelta
-from .. import database, templates, config, schemas
-from ..models import User_Accounts, Sessions
-from ..database import get_db
-from sqlalchemy.orm import Session
-from starlette.responses import RedirectResponse, HTMLResponse
-from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from typing import Annotated
+
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
 from passlib.context import CryptContext
+from pydantic import BaseModel
+
+# to get a string like this run:
+# openssl rand -hex 32
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 
-router = APIRouter(
-    prefix="/user", tags=["Users Frontend Testing"], include_in_schema=True
-)
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
+        "disabled": False,
+    }
+}
 
 
-@router.get("/", response_class=HTMLResponse)
-def feeds(request: Request, db: Session = Depends(get_db)):
-    return templates.TemplateResponse(
-        "user/user.html",
-        {"request": request, "user": User_Accounts.get_all_users(db)},
-    )
+class Token(BaseModel):
+    access_token: str
+    token_type: str
 
 
-@router.post("/login", name="Login")
-def login(request: schemas.Login, db: Session = Depends(get_db)):
-    # case insensitive email search
-    user = User_Accounts.get_user_by_username(db, request.username)
-    if user:
-        pw = request.password
-        if bcrypt.checkpw(pw.encode("utf-8"), user.password_hash.encode("utf-8")):
-            Sessions.create_session(db, user)
-            return {}
+class TokenData(BaseModel):
+    username: str | None = None
 
-    raise HTTPException(
+
+class User(BaseModel):
+    username: str
+    email: str | None = None
+    full_name: str | None = None
+    disabled: bool | None = None
+
+
+class UserInDB(User):
+    hashed_password: str
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+app = FastAPI()
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+
+def authenticate_user(fake_db, username: str, password: str):
+    user = get_user(fake_db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Invalid username and/or password",
+        detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},
     )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = get_user(fake_users_db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
 
 
-@router.delete("/logout", name="Logout")
-def logout(request: Request, db: Session = Depends(get_db)):
-    Session.deleteOne(id=request.user_session.id)
-    return "Logged out successfully"  # FIX ME
+async def get_current_active_user(
+    current_user: Annotated[User, Depends(get_current_user)]
+):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
+@app.post("/token", response_model=Token)
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
+):
+    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@app.get("/users/me/", response_model=User)
+async def read_users_me(
+    current_user: Annotated[User, Depends(get_current_active_user)]
+):
+    return current_user
+
+
+@app.get("/users/me/items/")
+async def read_own_items(
+    current_user: Annotated[User, Depends(get_current_active_user)]
+):
+    return [{"item_id": "Foo", "owner": current_user.username}]
