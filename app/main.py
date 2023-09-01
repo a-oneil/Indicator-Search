c30db0d335b3a5bed4551fe7af4429a0538839d6
Austin O'Neil <austin.oneil@redcanary.com>
2023-09-01 00:59:15 -0400

    little cleanup

--- app/main.py
+++ app/main.py
@@ -1,42 +1,21 @@
-import time
-from fastapi import FastAPI, Request, Depends
+from fastapi import FastAPI
 from starlette.staticfiles import StaticFiles
 
-from .models import Sessions, Base
-from .routers import indicators, iocs, feeds, users, indicators_api, iocs_api, feeds_api, users_api
-from .database import engine, get_db
-from sqlalchemy.orm import Session
+from .models import Base
+from .routers import (
+    indicators,
+    iocs,
+    feeds,
+    users,
+    indicators_api,
+    iocs_api,
+    feeds_api,
+    users_api,
+)
+from .database import engine
 
 # Create the FastAPI app
 app = FastAPI()
-
-
-@app.middleware("http")
-async def add_process_time_header(request: Request, call_next, db: Session = Depends(get_db)):
-    start_time = time.time()
-    db = next(get_db())
-    # Before response
-    response = await call_next(request)
-
-    # After response
-    try:
-        user_session = request.user_session
-    except AttributeError:
-        user_session = None
-        # No active user session
-        pass
-
-    if user_session:
-        # Refresh session
-        
-        session = db.query(Sessions).filter(Sessions.user_id == user_session.user_id).first()
-        if session:
-            response.headers["X-Authentication-JWT"] = session.encode_auth_token(db)
-
-    process_time = time.time() - start_time
-    response.headers["X-Process-Time"] = str(process_time)
-    return response
-
 
 # Create the database tables
 Base.metadata.create_all(engine)
