c906d80aeb6dbc73f8e87a2ebf42e7823ffaee47
Austin O'Neil <austin.oneil@redcanary.com>
2023-09-06 00:57:04 -0400

    indicators route cleanup

--- app/routers/indicators_api.py
+++ app/routers/indicators_api.py
@@ -4,16 +4,18 @@ from ..database import get_db
 from ..osint import new_indicator_handler, get_type
 from ..authentication import auth_api_key
 from sqlalchemy.orm import Session
-from fastapi import APIRouter, Depends, Request, BackgroundTasks
+from fastapi import APIRouter, Depends, BackgroundTasks, HTTPException
 
 
 router = APIRouter(prefix="/api")
 
 
-# fmt: off
 @router.post("/indicator", name="Create a new indicator", tags=["Indicators"])
-def create_indicator(request: schemas.CreateIndicator, background_tasks: BackgroundTasks, db: Session = Depends(get_db)):
-# fmt: on
+def create_indicator(
+    request: schemas.CreateIndicator,
+    background_tasks: BackgroundTasks,
+    db: Session = Depends(get_db),
+):
     auth_api_key(request, db)
     try:
         indicator = str(request.indicator).strip()
@@ -21,7 +23,9 @@ def create_indicator(request: schemas.CreateIndicator, background_tasks: Backgro
         if indicator_type:
             user = User_Accounts.get_user_by_api_key(request.api_key, db)
             new_indicator = Indicators(
-                indicator=indicator, indicator_type=indicator_type, username=user.username
+                indicator=indicator,
+                indicator_type=indicator_type,
+                username=user.username,
             )
             db.add(new_indicator)
             db.commit()
@@ -33,25 +37,29 @@ def create_indicator(request: schemas.CreateIndicator, background_tasks: Backgro
                 "Must be a valid IPv4 Address, IPv6 Address, Hash, FQDN, URL, Email, or MAC Address"
             )
     except Exception as e:
-        return {"Error": str(e)}
+        return HTTPException(400, str(e))
 
-# fmt: off
-@router.get("/indicator/{indicator_id}", name="Get results for an indicator", tags=["Indicators"])
+
+@router.get(
+    "/indicator/{indicator_id}",
+    name="Get results for an indicator",
+    tags=["Indicators"],
+)
 def get_indicator_results(indicator_id: int, db: Session = Depends(get_db)):
-# fmt: on
     try:
         indicator = Indicators.get_indicator_by_id(indicator_id, db)
         if indicator:
             return indicator
         else:
-            raise Exception("Indicator not found")
+            raise HTTPException(404, "Indicator not found")
     except Exception as e:
-        return {"Error": str(e)}
+        return HTTPException(400, str(e))
 
-# fmt: off
+
 @router.post("/indicator/search", name="Search for indicators", tags=["Indicators"])
-def search_for_indicator(request: schemas.SearchIndicators, db: Session = Depends(get_db)):
-# fmt: on
+def search_for_indicator(
+    request: schemas.SearchIndicators, db: Session = Depends(get_db)
+):
     try:
         results = Indicators.get_search_results(
             db,
@@ -65,14 +73,14 @@ def search_for_indicator(request: schemas.SearchIndicators, db: Session = Depend
             request.created_by,
         )
 
+        if not results:
+            raise HTTPException(404, "No results found")
         return results
-    except Exception:
-        return {"Error": "No results found"}
+    except Exception as e:
+        return HTTPException(400, str(e))
 
 
-# fmt: off
 @router.put("/indicator/notes", name="Add notes to an indicator", tags=["Indicators"])
-# fmt: on
 def add_indicator_notes(request: schemas.CreateNote, db: Session = Depends(get_db)):
     try:
         auth_api_key(request, db)
@@ -88,15 +96,17 @@ def add_indicator_notes(request: schemas.CreateNote, db: Session = Depends(get_d
                 "ioc_id": indicator.ioc_id,
             }
         else:
-            raise Exception("Indicator not found")
+            raise HTTPException(404, "Indicator not found")
     except Exception as e:
-        return {"Error": str(e)}
+        return HTTPException(400, str(e))
 
 
-# fmt: off
-@router.get("/indicator/notes/{indicator_id}", name="Get notes from an indicator", tags=["Indicators"])
-def get_notes(request: Request, indicator_id: int, db: Session = Depends(get_db)):
-# fmt: on
+@router.get(
+    "/indicator/notes/{indicator_id}",
+    name="Get notes from an indicator",
+    tags=["Indicators"],
+)
+def get_notes(indicator_id: int, db: Session = Depends(get_db)):
     try:
         indicator = Indicators.get_indicator_by_id(indicator_id, db)
         if indicator:
@@ -106,53 +116,52 @@ def get_notes(request: Request, indicator_id: int, db: Session = Depends(get_db)
                 "indicator_type": indicator.indicator_type,
                 "notes": indicator.notes,
                 "ioc_id": indicator.ioc_id,
-                }
+            }
         else:
-            raise Exception("Indicator not found")
+            raise HTTPException(404, "Indicator not found")
     except Exception as e:
-        return {"Error": str(e)}
+        return HTTPException(400, str(e))
 
 
-# fmt: off
-@router.delete("/indicator/{indicator_id}", name="Delete an indicator", tags=["Indicators"])
+@router.delete(
+    "/indicator/{indicator_id}", name="Delete an indicator", tags=["Indicators"]
+)
 def delete_indicator(request: schemas.DeleteIndicator, db: Session = Depends(get_db)):
-# fmt: on
     try:
         auth_api_key(request, db)
         indicator = Indicators.get_indicator_by_id(request.indicator_id, db)
-        if indicator:
+        if not indicator:
+            raise HTTPException(404, "Indicator not found")
+
+        ioc = Iocs.get_ioc_by_id(indicator.ioc_id, db)
+        if ioc:
+            ioc.indicator_id = None
+            db.add(ioc)
+            db.commit()
+        db.delete(indicator)
+        db.commit()
+        return HTTPException(200, f"Indicator {indicator.indicator} deleted")
+    except Exception as e:
+        return HTTPException(400, str(e))
+
+
+@router.delete("/indicator", name="Delete all indicators", tags=["Indicators"])
+def delete_all_indicators(
+    request: schemas.DeleteAllIndicators, db: Session = Depends(get_db)
+):
+    try:
+        auth_api_key(request, db)
+        indicators = Indicators.get_all_indicators(db)
+        if not indicators:
+            HTTPException(404, "No indicators found")
+        for indicator in indicators:
             ioc = Iocs.get_ioc_by_id(indicator.ioc_id, db)
             if ioc:
                 ioc.indicator_id = None
                 db.add(ioc)
-                db.commit()    
+                db.commit()
             db.delete(indicator)
             db.commit()
-        else:
-            raise Exception("Indicator not found")
-        
-        return {"Success": "Indicator deleted"}
+        return HTTPException(200, "All indicators deleted")
     except Exception as e:
-        return {"Error": str(e)}
-
-
-@router.delete("/indicator", name="Delete all indicators", tags=["Indicators"])
-def delete_all_indicators(request: schemas.DeleteAllIndicators, db: Session = Depends(get_db)):
-    try:
-        auth_api_key(request, db)
-        indicators = Indicators.get_all_indicators(db)
-        if indicators:
-            for indicator in indicators:
-                ioc = Iocs.get_ioc_by_id(indicator.ioc_id, db)
-                if ioc:
-                    ioc.indicator_id = None
-                    db.add(ioc)
-                    db.commit()    
-                db.delete(indicator)
-                db.commit()
-        else:
-            raise Exception("No indicators found")
-        
-        return {"Success": "All indicators deleted"}
-    except Exception as e:
-        return {"Error": str(e)}
+        return HTTPException(400, str(e))
