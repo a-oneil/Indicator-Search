fbfdc94e1ab2c17e53ad9876bb18b11d281c0cf9
Austin O'Neil <austin.oneil@redcanary.com>
2023-09-07 03:07:26 -0400

    fix json link

--- app/osint/_links.py
+++ app/osint/_links.py
@@ -9,14 +9,14 @@ def links_handler(indicator):
                 "virustotal": f"https://www.virustotal.com/gui/ip-address/{indicator.indicator}",
                 "abuseipdb": f"https://www.abuseipdb.com/check/{indicator.indicator}",
                 "greynoise": f"https://viz.greynoise.io/ip/{indicator.indicator}",
-                "json": f"{config['SERVER_ADDRESS']}/api/indicator/{indicator.id}",
+                "json": f"/api/indicator/{indicator.id}",
             },
         )
     if indicator.indicator_type == "ipv6":
         links.update(
             {
                 "abuseipdb": f"https://www.abuseipdb.com/check/{indicator.indicator}",
-                "json": f"{config['SERVER_ADDRESS']}/api/indicator/{indicator.id}",
+                "json": f"/api/indicator/{indicator.id}",
             },
         )
     elif indicator.indicator_type in [
@@ -30,7 +30,7 @@ def links_handler(indicator):
                 "virustotal": f"https://www.virustotal.com/gui/file/{indicator.indicator}",
                 "hybrid_analysis": f"https://www.hybrid-analysis.com/search?query={indicator.indicator}",
                 "joes_sandbox": f"https://www.joesandbox.com/search?q={indicator.indicator}",
-                "json": f"{config['SERVER_ADDRESS']}/api/indicator/{indicator.id}",
+                "json": f"/api/indicator/{indicator.id}",
             },
         )
     elif indicator.indicator_type == "fqdn":
@@ -39,7 +39,7 @@ def links_handler(indicator):
                 "virustotal": f"https://www.virustotal.com/gui/domain/{indicator.indicator}",
                 "mx_toolbox": f"https://mxtoolbox.com/SuperTool.aspx?action=mx%3a{indicator.indicator}&run=toolpage",
                 "urlscan.io": f"https://urlscan.io/search/#{indicator.indicator.replace('https://', '').replace('http://', '')}",
-                "json": f"{config['SERVER_ADDRESS']}/api/indicator/{indicator.id}",
+                "json": f"/api/indicator/{indicator.id}",
             },
         )
     elif indicator.indicator_type == "url":
@@ -48,7 +48,7 @@ def links_handler(indicator):
                 "virustotal": f"https://www.virustotal.com/gui/domain/{indicator.indicator}",
                 "mx_toolbox": f"https://mxtoolbox.com/SuperTool.aspx?action=mx%3a{indicator.indicator}&run=toolpage",
                 "urlscan.io": f"https://urlscan.io/search/#{indicator.indicator.replace('https://', '').replace('http://', '')}",
-                "json": f"{config['SERVER_ADDRESS']}/api/indicator/{indicator.id}",
+                "json": f"/api/indicator/{indicator.id}",
             },
         )
     elif indicator.indicator_type == "email":
@@ -56,14 +56,14 @@ def links_handler(indicator):
             {
                 "emailrep.io": f"https://emailrep.io/{indicator.indicator}",
                 "mx_toolbox": f"https://mxtoolbox.com/SuperTool.aspx?action=mx%3a{indicator.indicator.split('@')[1]}&run=toolpage",
-                "json": f"{config['SERVER_ADDRESS']}/api/indicator/{indicator.id}",
+                "json": f"/api/indicator/{indicator.id}",
             },
         )
     elif indicator.indicator_type == "mac":
         links.update(
             {
                 "mac_vendor_lookup": f"https://api.macvendors.com/{indicator.indicator}",
-                "json": f"{config['SERVER_ADDRESS']}/api/indicator/{indicator.id}",
+                "json": f"/api/indicator/{indicator.id}",
             },
         )
 
