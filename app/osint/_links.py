6b448d28b0079688eb3f69ec6ecc755d8708e1b5
Austin O'Neil <austin.oneil@redcanary.com>
2023-11-12 19:19:13 -0500

    add twitter search quick link

--- app/osint/_links.py
+++ app/osint/_links.py
@@ -10,6 +10,7 @@ def links_handler(indicator):
                 "abuseipdb": f"https://www.abuseipdb.com/check/{indicator.indicator}",
                 "greynoise": f"https://viz.greynoise.io/ip/{indicator.indicator}",
                 "google": f"https://www.google.com/search?q={indicator.indicator}",
+                "twitter": f"https://twitter.com/search?q={indicator.indicator}&src=typed_query",
                 "json": f"/api/indicator/{indicator.id}",
             },
         )
@@ -18,6 +19,7 @@ def links_handler(indicator):
             {
                 "abuseipdb": f"https://www.abuseipdb.com/check/{indicator.indicator}",
                 "google": f"https://www.google.com/search?q={indicator.indicator}",
+                "twitter": f"https://twitter.com/search?q={indicator.indicator}&src=typed_query",
                 "json": f"/api/indicator/{indicator.id}",
             },
         )
@@ -31,8 +33,9 @@ def links_handler(indicator):
             {
                 "virustotal": f"https://www.virustotal.com/gui/file/{indicator.indicator}",
                 "hybrid_analysis": f"https://www.hybrid-analysis.com/search?query={indicator.indicator}",
-                "joes_sandbox": f"https://www.joesandbox.com/search?q={indicator.indicator}",
+                "joes_sandbox": f"https://www.joesandbotwitter/search?q={indicator.indicator}",
                 "google": f"https://www.google.com/search?q={indicator.indicator}",
+                "twitter": f"https://twitter.com/search?q={indicator.indicator}&src=typed_query",
                 "json": f"/api/indicator/{indicator.id}",
             },
         )
@@ -40,9 +43,10 @@ def links_handler(indicator):
         links.update(
             {
                 "virustotal": f"https://www.virustotal.com/gui/domain/{indicator.indicator}",
-                "mx_toolbox": f"https://mxtoolbox.com/SuperTool.aspx?action=mx%3a{indicator.indicator}&run=toolpage",
+                "mx_toolbox": f"https://mxtoolbotwitter/SuperTool.aspx?action=mx%3a{indicator.indicator}&run=toolpage",
                 "urlscan.io": f"https://urlscan.io/search/#{indicator.indicator.replace('https://', '').replace('http://', '')}",
                 "google": f"https://www.google.com/search?q={indicator.indicator}",
+                "twitter": f"https://twitter.com/search?q={indicator.indicator}&src=typed_query",
                 "json": f"/api/indicator/{indicator.id}",
             },
         )
@@ -50,9 +54,10 @@ def links_handler(indicator):
         links.update(
             {
                 "virustotal": f"https://www.virustotal.com/gui/domain/{indicator.indicator}",
-                "mx_toolbox": f"https://mxtoolbox.com/SuperTool.aspx?action=mx%3a{indicator.indicator}&run=toolpage",
+                "mx_toolbox": f"https://mxtoolbotwitter/SuperTool.aspx?action=mx%3a{indicator.indicator}&run=toolpage",
                 "urlscan.io": f"https://urlscan.io/search/#{indicator.indicator.replace('https://', '').replace('http://', '')}",
                 "google": f"https://www.google.com/search?q={indicator.indicator}",
+                "twitter": f"https://twitter.com/search?q={indicator.indicator}&src=typed_query",
                 "json": f"/api/indicator/{indicator.id}",
             },
         )
@@ -60,8 +65,9 @@ def links_handler(indicator):
         links.update(
             {
                 "emailrep.io": f"https://emailrep.io/{indicator.indicator}",
-                "mx_toolbox": f"https://mxtoolbox.com/SuperTool.aspx?action=mx%3a{indicator.indicator.split('@')[1]}&run=toolpage",
+                "mx_toolbox": f"https://mxtoolbotwitter/SuperTool.aspx?action=mx%3a{indicator.indicator.split('@')[1]}&run=toolpage",
                 "google": f"https://www.google.com/search?q={indicator.indicator}",
+                "twitter": f"https://twitter.com/search?q={indicator.indicator}&src=typed_query",
                 "json": f"/api/indicator/{indicator.id}",
             },
         )
@@ -70,14 +76,16 @@ def links_handler(indicator):
             {
                 "mac_vendor_lookup": f"https://api.macvendors.com/{indicator.indicator}",
                 "google": f"https://www.google.com/search?q={indicator.indicator}",
+                "twitter": f"https://twitter.com/search?q={indicator.indicator}&src=typed_query",
                 "json": f"/api/indicator/{indicator.id}",
             },
         )
     elif indicator.indicator_type == "phone":
         links.update(
             {
-                "ip_quality_score": f"https://www.ipqualityscore.com/free-phone-number-lookup",
+                "ip_quality_score": "https://www.ipqualityscore.com/free-phone-number-lookup",
                 "google": f"https://www.google.com/search?q={indicator.indicator}",
+                "twitter": f"https://twitter.com/search?q={indicator.indicator}&src=typed_query",
                 "json": f"/api/indicator/{indicator.id}",
             },
         )
