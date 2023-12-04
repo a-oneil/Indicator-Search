90546ecc1c90ac2e9a9d62cf67c68723cda3ae61
Austin O'Neil <austin.oneil@redcanary.com>
2023-12-04 00:51:54 -0500

    continuing to build out dev framework

--- .tool_dev.py
+++ .tool_dev.py
@@ -1,7 +1,10 @@
 import json
 import random
+import argparse
+import traceback
 import app.osint.tools as tools
 from datetime import datetime
+from app import color
 from app.osint import tagging_handler, links_handler, enrichments_handler
 from app.database import SessionManager
 from app.osint.utils import (
@@ -51,6 +54,9 @@ class TestIndicator:
         """
         self.run_tools()
         print(json.dumps(self.results, indent=4))
+        print(
+            f"\n{color.BLUE}The JSON above are the indicator results for reference.{color.ENDCOLOR}"
+        )
         self.tags = tagging_handler(self)
         return self.tags
 
@@ -66,6 +72,10 @@ class TestIndicator:
         Add enrichments to the indicator
         """
         self.run_tools()
+        print(json.dumps(self.results, indent=4))
+        print(
+            f"\n{color.BLUE}The JSON above are the indicator results for reference.{color.ENDCOLOR}"
+        )
         self.enrichments = enrichments_handler(self)
         return self.enrichments
 
@@ -73,7 +83,6 @@ class TestIndicator:
         """
         Add feedlist results to the indicator
         """
-
         self.feedlist_results = tools.search_feedlists(
             self,
             self.db,
@@ -81,32 +90,109 @@ class TestIndicator:
         return self.feedlist_results
 
 
-def print_and_write_output(data):
-    print(json.dumps(data, indent=4))
-    with open("./.tool_dev.json", "w") as outfile:
-        json.dump(data, outfile, indent=4)
+def print_and_write_output(data, args):
+    def print_argument_names(args):
+        for arg_name, arg_value in vars(args).items():
+            if arg_value != parser.get_default(arg_name):
+                print(f"{arg_name} = {arg_value}")
+
+    if data:
+        print(f"\n{color.BLUE}==== Testing output ===={color.ENDCOLOR}\n")
+        print(json.dumps(data, indent=4))
+        with open("./.tool_dev.json", "w") as outfile:
+            json.dump(data, outfile, indent=4)
+    else:
+        print(f"\n{color.RED}No output using the following arguments:{color.ENDCOLOR}")
+        print_argument_names(args)
 
 
-with SessionManager() as db:
-    indicator = TestIndicator(
-        indicator="00sms.xyz",
-        db=db,
+def call_function_if_exists(function_name, indicator):
+    if hasattr(tools, function_name) and callable(getattr(tools, function_name)):
+        func = getattr(tools, function_name)
+        return func(indicator)
+    else:
+        available_functions = [
+            func for func in dir(tools) if callable(getattr(tools, func))
+        ]
+        print(f"{color.YELLOW}Available tools to run:{color.ENDCOLOR}")
+        print("\n".join(available_functions))
+        return None
+
+
+try:
+    parser = argparse.ArgumentParser(
+        description="Indicator Search Tool Dev Framework",
+        formatter_class=argparse.RawTextHelpFormatter,
+    )
+    parser.add_argument(
+        "--indicator",
+        action="store",
+        help="Pass an indicator to test with",
+        required=True,
+    )
+    parser.add_argument(
+        "--tool",
+        action="store",
+        help="Enter a specific tool name to run from the tools directory",
+    )
+    parser.add_argument(
+        "--all",
+        action="store_true",
+        help="Run all tools against an indicator",
+    )
+    parser.add_argument(
+        "--tags",
+        action="store_true",
+        help="Get tags for an indicator",
+    )
+    parser.add_argument(
+        "--links",
+        action="store_true",
+        help="Get links for an indicator",
+    )
+    parser.add_argument(
+        "--enrichments",
+        action="store_true",
+        help="Get enrichments for an indicator",
+    )
+    parser.add_argument(
+        "--feedlists",
+        action="store_true",
+        help="Get feedlist results for an indicator",
     )
 
-    # Run a specific tool
-    # print_and_write_output(tools.malware_bazzar(indicator))
+    if parser.parse_args().indicator:
+        indicator = parser.parse_args().indicator
+    else:
+        indicator = input("Enter an indicator to search for: ")
 
-    # Run all tools
-    # print_and_write_output(indicator.run_tools())
+    with SessionManager() as db:
+        # fmt: off
+        indicator = TestIndicator(indicator=indicator, db=db)
 
-    # Run tagging
-    # print_and_write_output(indicator.run_tagging())
+        if parser.parse_args().tool:
+            print_and_write_output(
+                call_function_if_exists(parser.parse_args().tool.strip(), indicator), parser.parse_args(),
+            )
 
-    # Get external links
-    # print_and_write_output(indicator.get_links())
+        elif parser.parse_args().all:
+            print_and_write_output(indicator.run_tools(), parser.parse_args())
 
-    # Get enrichments
-    # print_and_write_output(indicator.get_enrichments())
+        elif parser.parse_args().tags:
+            print_and_write_output(indicator.run_tagging(), parser.parse_args())
 
-    # Get feedlist results
-    print_and_write_output(indicator.get_feedlist_results())
+        elif parser.parse_args().links:
+            print_and_write_output(indicator.get_links(), parser.parse_args())
+
+        elif parser.parse_args().enrichments:
+            print_and_write_output(indicator.get_enrichments(), parser.parse_args())
+        
+        elif parser.parse_args().feedlists:
+            print_and_write_output(indicator.get_feedlist_results(), parser.parse_args())
+        
+        else:
+            print_and_write_output(indicator.run_tools(), parser.parse_args())
+        # fmt: on
+
+except Exception:
+    traceback.print_exc()
