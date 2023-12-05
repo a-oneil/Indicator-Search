4f735b7b8f8b95df39e2179b8618574812490cb8
Austin O'Neil <austin.oneil@redcanary.com>
2023-12-05 18:18:01 -0500

    fix crashing when maltiverse is unreachable

--- app/osint/tools/_maltiverse.py
+++ app/osint/tools/_maltiverse.py
@@ -1,5 +1,5 @@
+import requests
 from ... import config
-from maltiverse import Maltiverse
 from ..utils import (
     no_results_found,
     failed_to_run,
@@ -12,34 +12,44 @@ def maltiverse(indicator):
         if config["MALTIVERSE_API_KEY"] == "":
             return missing_apikey("maltiverse")
 
-        maltiverse = Maltiverse(auth_token=config["MALTIVERSE_API_KEY"])
+        header = {"X-API-Key": config["MALTIVERSE_API_KEY"]}
+        api = "https://api.maltiverse.com"
 
         if indicator.indicator_type == "hash.md5":
-            result = maltiverse.sample_get_by_md5(indicator.indicator)
+            result = requests.get(
+                f"{api}/sample/md5/{indicator.indicator}", headers=header
+            )
 
         elif indicator.indicator_type == "hash.sha1":
-            result = maltiverse.sample_get_by_sha1(indicator.indicator)
+            result = requests.get(
+                f"{api}/sample/sha1/{indicator.indicator}", headers=header
+            )
 
         elif indicator.indicator_type == "hash.sha256":
-            result = maltiverse.sample_get_by_sha256(indicator.indicator)
+            result = requests.get(f"{api}/sample/{indicator.indicator}", headers=header)
 
         elif indicator.indicator_type == "hash.sha512":
-            result = maltiverse.sample_get_by_sha512(indicator.indicator)
+            result = requests.get(
+                f"{api}/sample/sha512/{indicator.indicator}", headers=header
+            )
 
         elif indicator.indicator_type == "fqdn":
-            result = maltiverse.hostname_get(indicator.indicator)
+            result = requests.get(
+                f"{api}/hostname/{indicator.indicator}", headers=header
+            )
 
         elif indicator.indicator_type == "ipv4":
-            result = maltiverse.ip_get(indicator.indicator)
-
-        elif indicator.indicator_type == "url":
-            result = maltiverse.url_get(indicator.indicator)
+            result = requests.get(f"{api}/ip/{indicator.indicator}", headers=header)
 
         else:
             raise Exception("Invalid indicator type for maltiverse")
 
-        if not result:
-            raise no_results_found("maltiverse")
+        if result.status_code != 200:
+            return failed_to_run(
+                tool_name="maltiverse",
+                error_message=result.reason,
+                status_code=result.status_code,
+            )
 
         if not result.get("classification", ""):
             return no_results_found("maltiverse")
@@ -52,18 +62,20 @@ def maltiverse(indicator):
             return no_results_found("maltiverse")
 
         return (
-            # fmt: off
-                {
-                    "tool": "maltiverse",
-                    "outcome": {"status": "results_found", "error_message": None, "status_code": None, "reason": None},
-                    "results": {
-                        "classification": result.get("classification", ""),
-                        "blacklist": result.get("blacklist", []),
-                        "tags": result.get("tag", []),
-
-                        },
+            {
+                "tool": "maltiverse",
+                "outcome": {
+                    "status": "results_found",
+                    "error_message": None,
+                    "status_code": None,
+                    "reason": None,
                 },
-            # fmt: on
+                "results": {
+                    "classification": result.get("classification", ""),
+                    "blacklist": result.get("blacklist", []),
+                    "tags": result.get("tag", []),
+                },
+            },
         )
     except Exception as error_message:
         return failed_to_run(tool_name="maltiverse", error_message=error_message)
